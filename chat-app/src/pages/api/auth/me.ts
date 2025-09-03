100485b27c474ea57afc083468d8d7c2cb5d67a4
MigManMebell <migman.mebel@yandex.ru>
2025-09-03 15:40:51 +0300

    Initial upload of project files

--- chat-app/src/pages/api/auth/me.ts
+++ chat-app/src/pages/api/auth/me.ts
@@ -1,8 +1,11 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { withAuth } from '../../../utils/auth';
 
-export default withAuth(async function handler(req: NextApiRequest & { user?: { userId: string; nickname?: string } }, res: NextApiResponse) {
-  return res.status(200).json({ userId: req.user!.userId, nickname: req.user!.nickname });
+export default withAuth(async function handler(
+  req: NextApiRequest & { user?: { userId: string; nickname?: string } },
+  res: NextApiResponse
+): Promise<void> {
+  res.status(200).json({ userId: req.user!.userId, nickname: req.user!.nickname });
 });
 
 
